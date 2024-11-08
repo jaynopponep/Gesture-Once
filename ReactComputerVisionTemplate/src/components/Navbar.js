235be61fbd74c636d7794a31fec62ba4fe2e9b22
Loyd <us.flores.loyd@gmail.com>
2024-11-07 22:48:47 -0500

    Frontend routing and template set, navbar created, interpret page implemented, about page created, home page created

---
+++ ReactComputerVisionTemplate/src/components/Navbar.js
@@ -0,0 +1,27 @@
+// src/components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/realtime">Interpret</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/learn">Learn</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
